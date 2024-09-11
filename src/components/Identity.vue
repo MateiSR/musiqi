741557d974fa0756d2b7eaac4bb67b706eebb3d0
MateiSR <MateiSR@proton.me>
2024-09-11 22:06:30 +0300

    Update logo & main page

--- src/components/Identity.vue
+++ src/components/Identity.vue
@@ -1,7 +1,49 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex flex-col justify-center items-center gap-4">
-    <img src="../assets/musiqi.svg" alt="musiqi" class="w-32 h-32 logo hover:animate-spin" />
+  <div class="flex flex-col justify-center items-center gap-4 pb-24">
+    <svg
+      version="1.0"
+      xmlns="http://www.w3.org/2000/svg"
+      width="167.000000pt"
+      height="131.000000pt"
+      viewBox="0 0 167.000000 131.000000"
+      preserveAspectRatio="xMidYMid meet"
+    >
+      <!-- Define the gradient -->
+      <defs>
+        <linearGradient id="gradient1" gradientTransform="rotate(135)">
+          <stop offset="0%" stop-color="#6EE7B7" />
+          <!-- Cyan -->
+          <stop offset="50%" stop-color="#FDE68A" />
+          <!-- Yellow -->
+          <stop offset="100%" stop-color="#F472B6" />
+          <!-- Pink -->
+        </linearGradient>
+      </defs>
+
+      <g
+        transform="translate(0.000000,131.000000) scale(0.100000,-0.100000)"
+        fill="url(#gradient1)"
+        stroke="none"
+      >
+        <path
+          d="M563 938 l-283 -283 280 -280 280 -280 280 278 c154 152 280 280 280
+    283 0 2 -125 131 -277 285 l-278 280 -282 -283z m483 22 l200 -200 -41 -40
+    -41 -40 -159 160 c-87 88 -160 160 -162 160 -2 0 -99 -94 -216 -209 l-212
+    -209 -36 31 c-21 18 -38 36 -38 41 -1 9 487 502 499 505 3 0 96 -89 206 -199z
+    m54 -345 l-40 -40 -108 108 -108 108 -108 -106 -108 -107 -39 38 -39 38 147
+    148 148 148 147 -147 148 -148 -40 -40z m-160 -160 l-95 -95 -95 95 -96 96 95
+    94 96 95 95 -95 95 -95 -95 -95z m373 237 l37 -38 -93 -92 -93 -92 -39 40 -39
+    40 89 90 c49 50 92 90 95 90 3 0 23 -17 43 -38z m-749 -101 l38 -39 -41 -41
+    -41 -41 -41 40 -41 39 39 39 c21 22 40 40 43 41 3 0 23 -17 44 -38z m176 -177
+    l103 -105 109 108 108 108 40 -40 40 -40 -150 -150 -151 -150 -145 149 -145
+    149 37 38 c21 22 41 39 44 39 3 0 53 -48 110 -106z"
+        />
+      </g>
+    </svg>
+
     <h1 class="text-5xl font-bold">Musiqi</h1>
+
+    <h2>The <b>YouTube playlist music quiz</b> game</h2>
   </div>
 </template>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,7 +1,25 @@
 <script setup lang="ts"></script>
 
+<!-- TODO: Fix FOUC if it happens on production -->
 <template>
-  <div class="flex flex-col min-h-screen justify-center items-center gap-4">
-    Test
+  <div class="flex flex-col pt-4 justify-center items-center gap-4">
+    <!-- Router link to start a lobby -->
+    <router-link to="/play" class="btn btn-primary">Create lobby</router-link>
+    <!-- Router link to play solo -->
+    <router-link to="/play/solo" class="btn btn-secondary"
+      >Play solo</router-link
+    >
+    <h3 class="text-slate-300">
+      Click
+      <a
+        href="https://www.youtube.com/feed/playlists"
+        class="text-blue-400 italic"
+        target="_blank"
+        rel="noopener noreferrer"
+        >here</a
+      >
+      to view your playlists
+    </h3>
+    <div class="flex gap-4"></div>
   </div>
 </template>
